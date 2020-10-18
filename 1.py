2b15b45b286ab7ad5e0d4241fece50e5dec270d0
Andrey <andreygora18@gmail.com>
2020-10-18 13:48:14 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,2 +1,15 @@
-g = input()
-print(g)
+while True :
+   k = input("Write your number")
+   h = input("It is not your number\n if You think it is bag write 1")
+   if h == 1:
+    break
+while True :
+   k = input("Write your email")
+   g = input("It is not your email\n if You think it is bag write 1")
+   if g == 1:
+    break
+while True :
+   k = input("Write your address")
+   w = input("It is not your address\n if You think it is bag write 1")
+   if w == 1:
+    break
